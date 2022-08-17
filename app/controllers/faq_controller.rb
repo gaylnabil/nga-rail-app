d2332afa3b0f42dea7eb7de438dbaece7fcafc70
Nabil GAYL <gaylnabil@gmail.com>
2022-08-17 16:03:17 +0100

    new line of user in FAQ
    Getting to FAQ page with User already logged in

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,5 +1,6 @@
 class FaqController < ApplicationController
   def index
     @faq_value = "This is the index page for FaqController Test"
+    @user = User.find_by(email: params[:email]) # this is the user that will be logged in"
   end
 end
